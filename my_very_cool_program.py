263a0af7036581f9ac6d022375990c045d88ef3a
denis <Denis.Gorshkov76@gmail.com>
2019-08-27 23:52:14 +0300

    hotfix

--- my_very_cool_program.py
+++ my_very_cool_program.py
@@ -1,3 +1,15 @@
 name = input("Введите имя: ")
 second_name = input("Введите фамилию:")
 print(f"hello {second_name} {name}")
+
+name = input("Введите имя: ")
+second_name = input("Введите фамилию:")
+print(f"hello {second_name} {name}")
+
+name = input("Введите имя: ")
+second_name = input("Введите фамилию:")
+print(f"hello {second_name} {name}")
+
+name = input("Введите имя: ")
+second_name = input("Введите фамилию:")
+print(f"hello {second_name} {name}")
